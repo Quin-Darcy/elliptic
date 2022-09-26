57b016cbe1646d6182b8a969b3ab13b768a21bdc
Quin Darcy <quintondarcy@csus.edu>
2022-09-25 22:21:52 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,45 @@
-use rand::Rng;
+mod utils;
 
-pub fn gen_curve(n: &u128) -> Vec<[u128; 2]> {
-    let p: u128 = (*n).clone();
-    let coeffs: [u128; 2] = get_coeffs(&p);
-    let squares: Vec<Vec<u128>> = get_squares(&p);
+// Given a prime p and coefficients (A, B) which define an elliptic curve E: x^3 + Ax^2 + B,
+// this function returns a point on this curve.
+pub fn get_curve_point(curve_coeffs: (u128, u128), p: u128) -> (u128, u128) {
+    let mut x: u128;
+    let mut y: u128;
+    let mut z: u128;
 
-    get_curve(&p, coeffs, squares)
-}
-
-fn get_coeffs(p: &u128) -> [u128; 2] {
-    let mut a: u128;
-    let mut b: u128;
-    let mut c: u128;
-    let coeffs: [u128; 2];
     loop {
-        a = rand::thread_rng().gen_range(0..*p);
-        b = rand::thread_rng().gen_range(0..*p);
-        c = 4*a.pow(3) + 27*b.pow(2);
-
-        if c % p != 0 {
-            coeffs = [a, b];
-            return coeffs;
-        }
-    }
-}
-
-fn get_squares(p: &u128) -> Vec<Vec<u128>> {
-    let mut index: usize;
-    let mut squares: Vec<Vec<u128>> = Vec::new();
-    for _i in 0..*p {
-        squares.push(Vec::new());
-    }
-    for x in 0..*p {
-        index = ((x as u128).pow(2) % p) as usize;
-        squares[index].push(x as u128);
-    }
-    squares
-}
-
-fn get_curve(p: &u128, coeffs: [u128; 2], squares: Vec<Vec<u128>>) -> Vec<[u128; 2]> {
-    let mut index: usize;
-    let mut curve: Vec<[u128; 2]> = Vec::new();
-    for x in 0..*p {
-        index = (((x as u128).pow(3) + coeffs[0]*(x as u128) + coeffs[1]) % p) as usize;
-        if squares[index].len() > 0 {
-            for y in &squares[index] {
-                curve.push([x as u128, *y]);
+        x = rand::thread_rng().gen_range(0..p);
+        z = (utils::tools::power(x, 3_u128, p) + curve_coeffs.0*x + curve_coeffs.1) % p;
+        loop {
+            if utils::tools::quadratic_residue(z, p) {
+                break;
+            } else {
+                x = rand::thread_rng().gen_range(0..p);
+                z = (utils::tools::power(x, 3_u128, p) + curve_coeffs.0*x + curve_coeffs.1) % p;
             }
         }
+        y = utils::tools::mod_sqrt(z, p);
+        if y != 1 && y != 0{
+            break;
+        }
     }
-    curve
+    (x, y)
+}
+
+pub fn order(point: (u128, u128), curve_coeffs: (u128, u128), p: u128) -> u128 {
+    let mut i: u128 = 0;
+    let mut lambda: u128;
+    let mut sum: (u128, u128) = (0, 0);
+    let mut x: u128 = point.0.clone();
+    let mut y: u128 = point.1.clone();
+    
+    while sum != (p, p) {
+        sum = utils::tools::add((x, y), point, curve_coeffs.0, p);
+        x = sum.0;
+        y = sum.1;
+        i += 1;
+    }
+    i
 }
 
 
