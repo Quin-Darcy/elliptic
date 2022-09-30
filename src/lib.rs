7f3ab212cf7f17eecfe194dc3792057cdab33aa4
Quin Darcy <quintondarcy@csus.edu>
2022-09-29 21:30:47 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -78,3 +78,63 @@ pub fn get_ec_point(curve_coeffs: &(BigUint, BigUint), p: &BigUint) -> (BigUint,
     }
     (x, y)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn modular_inverse_test() {
+        assert_eq!(3*utils::mod_inv(&BigUint::from(3_u32), BigUint::from(13_u32)) % 13_u32, One::one());
+        assert_eq!(5*utils::mod_inv(5, 41) % 41, 1);
+    }
+
+    #[test]
+    fn modular_subtraction_test() {
+        assert_eq!(utils::mod_sub(5, 11, 13), 7);
+        assert_eq!(utils::mod_sub(9, 2, 41), 7);
+    }
+
+    #[test]
+    fn quadriatic_residue_test() {
+        assert_eq!(utils::quadratic_residue(12, 13), true);
+    }
+
+    #[test]
+    fn tonelli_shanks_test() {
+        let root: u128 = utils::tonelli_shanks(5, 41);
+        assert_eq!(root == 13 || root == 28, true);
+    }
+
+    #[test]
+    fn modular_square_root_test() {
+        let root: u128 = utils::mod_sqrt(12, 13);
+        assert_eq!(utils::power(root, 2, 13), 12);
+    }
+
+    #[test]
+    fn elliptic_curve_addition_test() {
+        assert_eq!(ec_add((9, 7), (2, 10), 3, 13), (12, 11));
+        assert_eq!(ec_add((17, 11), (39, 24), 17, 41), (10, 36));
+    }
+
+    #[test]
+    fn elliptic_curve_coefficients_test() {
+        let coeffs: (u128, u128) = get_ec_coeffs(13);
+        let term: u128 = 4*utils::power(coeffs.0, 3, 13)
+            +27*utils::power(coeffs.1, 2, 13);
+        
+        assert_eq!(term % 13 == 0, false);
+    }
+
+    #[test]
+    fn elliptic_curve_point_test() {
+        let curve_coeffs: (u128, u128) = get_ec_coeffs(13);
+        let point: (u128, u128) = get_ec_point(curve_coeffs, 13);
+        let lhs: u128 = utils::power(point.1, 2, 13);
+        let rhs: u128 = utils::power(point.0, 3, 13)
+            +curve_coeffs.0*point.0+curve_coeffs.1;
+        
+        assert_eq!(lhs, rhs % 13);
+    }
+}
