b3df71d5a65d51b79e55b017f595bbc2e71e4860
Quin Darcy <quintondarcy@csus.edu>
2022-09-29 21:18:20 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,80 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
 use rand::Rng;
-use num_bigint::{BigUint, RandBigInt};
+use num_traits::{Zero, One};
 use num_traits::cast::ToPrimitive;
-use num_traits::identities::{Zero};
+use num_bigint::{BigUint, RandBigInt};
+mod utils;
 
 
-pub fn gen_curve(n: BigUint) -> Vec<[BigUint; 2]> {
-    let p: BigUint = n.clone();
-    let coeffs: [BigUint; 2] = get_coeffs(&p);
-    let squares: Vec<Vec<BigUint>> = get_squares(&p);
-    
-    get_curve(&p, coeffs, squares)
+pub fn ec_add(P: &(BigUint, BigUint), Q: &(BigUint, BigUint), a: &BigUint, p: &BigUint) -> (BigUint, BigUint) {
+    let x1: &BigUint = &P.0; let y1: &BigUint = &P.1;
+    let x2: &BigUint = &Q.0; let y2: &BigUint = &Q.1;
+    let x3: BigUint; let y3: BigUint;
+    let lambda: BigUint;
+
+    if x1 == p || y1 == p {
+        return ((*x2).clone(), (*y2).clone());
+    } 
+    if x2 == p || y2 == p {
+        return ((*x1).clone(), (*y1).clone());
+    }
+    if x1 == x2 && y1 == &(p-y2) {
+        return ((*p).clone(), (*p).clone());
+    }
+
+    if x1 == x2 && y1 == y2 {
+        lambda = ((3_u32*x1.modpow(&BigUint::from(2_u32), &p)+a)*utils::mod_inv(&(BigUint::from(2_u32)*y1), p)) % p; 
+    } else {
+        lambda = (utils::mod_sub(&y2, &y1, p)*utils::mod_inv(&utils::mod_sub(&x2, &x1, p), p)) % p; 
+    }
+
+    x3 = utils::mod_sub(&lambda.modpow(&BigUint::from(2_u32), p), &(x1+x2), p); 
+    y3 = utils::mod_sub(&(lambda*utils::mod_sub(&x1, &x3, p)), &y1, p);
+
+    (x3, y3)
 }
 
-fn get_coeffs(p: &BigUint) -> [BigUint; 2] {
-    let mut a: BigUint;
-    let mut b: BigUint;
-    let mut c: BigUint;
-    let coeffs: [BigUint; 2];
-    loop {
-        a = rand::thread_rng().gen_biguint_range(&BigUint::zero(), &p.clone());
-        b = rand::thread_rng().gen_biguint_range(&BigUint::zero(), &p.clone());
-        c = (4_u32)*BigUint::pow(&a, 3_u32) + (27_u32)*BigUint::pow(&b, 2_u32);
+pub fn get_ec_coeffs(p: &BigUint) -> (BigUint, BigUint) {
+    let mut a: BigUint = rand::thread_rng().gen_biguint_range(&Zero::zero(), &p);
+    let mut b: BigUint = rand::thread_rng().gen_biguint_range(&Zero::zero(), &p);
 
-        if !(c % p.clone()).is_zero() {
-            coeffs = [a, b];
-            return coeffs;
+    loop {
+        let coeff_cond: BigUint = 4_u32*a.modpow(&BigUint::from(3_u32), &p)
+                                    +27_u32*b.modpow(&BigUint::from(2_u32), &p);
+        
+        if coeff_cond % p != Zero::zero() {
+            break;
+        } else {
+            a = rand::thread_rng().gen_biguint_range(&Zero::zero(), &p);
+            b = rand::thread_rng().gen_biguint_range(&Zero::zero(), &p);
         }
     }
+    (a, b)
 }
 
-fn get_squares(p: &BigUint) -> Vec<Vec<BigUint>> {
-    let mut index: usize;
-    let mut squares: Vec<Vec<BigUint>> = Vec::new();
-    let mut i: BigUint = BigUint::zero();
-    
-    while i < *p {
-        squares.push(Vec::new());
-        i += BigUint::from(1_u32);
-    }
-    i = BigUint::zero();
-    
-    while i < *p {
-        index = BigUint::modpow(&i, &BigUint::from(2_u32), p).to_usize().unwrap();
-        squares[index].push(i.clone());
-        i += BigUint::from(1_u32);
-    }
-    squares
-}
+pub fn get_ec_point(curve_coeffs: &(BigUint, BigUint), p: &BigUint) -> (BigUint, BigUint) {
+    let mut x: BigUint;
+    let mut y: BigUint;
+    let mut z: BigUint;
 
-fn get_curve(p: &BigUint, coeffs: [BigUint; 2], squares: Vec<Vec<BigUint>>) -> Vec<[BigUint; 2]> {
-    let mut index: usize;
-    let mut curve: Vec<[BigUint; 2]> = Vec::new();
-    let mut x: BigUint = BigUint::zero();
-
-    while x < *p {
-        index = ((x.pow(3) + coeffs[0].clone()*&x + coeffs[1].clone()) % p).to_usize().unwrap();
-        if squares[index].len() > 0 {
-            for y in &squares[index] {
-                curve.push([x.clone(), (*y).clone()]);
+    loop {
+        x = rand::thread_rng().gen_biguint_range(&Zero::zero(), &p);
+        z = (x.modpow(&BigUint::from(3_u32), &p)+&curve_coeffs.0*&x+&curve_coeffs.1) % p; 
+        loop {
+            if utils::quadratic_residue(&z, &p) {
+                break;
+            } else {
+                x = rand::thread_rng().gen_biguint_range(&Zero::zero(), &p);
+                z = (x.modpow(&BigUint::from(3_u32), &p)+&curve_coeffs.0*&x+&curve_coeffs.1) % p;
             }
         }
-        x += BigUint::from(1_u32);
+        y = utils::mod_sqrt(&z, &p);
+        if y != One::one() && y != Zero::zero() {
+            break;
+        }
     }
-    curve
+    (x, y)
 }
